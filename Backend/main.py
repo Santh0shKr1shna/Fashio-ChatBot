eaa3e0d7d8f7f25db5787f68326541cc97b1ed19
shershon <shershon.japs@gmail.com>
2023-08-20 12:04:25 +0530

    Login backend + frontend

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from fastapi import FastAPI, Request, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
 import subprocess
 import os
@@ -18,6 +19,18 @@ VTON_IMG_UPLOAD_DIR = "./Virtual_TON"
 app=FastAPI()
 
 
+# Configure CORS settings
+origins = [
+    "http://localhost:3000",  # Replace with your React app's URL
+    "https://your-react-app-domain.com"  # Add more origins if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 #image holders
 class ImageData(BaseModel):
     image1: str
@@ -25,7 +38,9 @@ class ImageData(BaseModel):
 
 
 @app.post('/login')
-def index(uname, pwd):
+async def index(data: dict):
+    uname= data.get('uname')
+    pwd=data.get('pwd')
     if not uname and not pwd:
         raise HTTPException(status_code=400, detail="Empty data fields")
     database=db.DataBase()
